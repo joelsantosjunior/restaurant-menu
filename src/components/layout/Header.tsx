30f3a4ac417003bcd414fb41b9768db7b294a2db
Joel Santos <joel.junior@actdigital.com>
2024-06-18 09:54:21 -0300

    add a top left button to enable language switching

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components'
+import LanguageToggle from '../ui/LanguageToggle'
 
 const LogoImg = styled.img`
   margin-top: 2em;
@@ -11,11 +12,13 @@ const MyHeader = styled.header`
   display: flex;
   justify-content: center;
   margin: 3em 0;
+  position: relative;
 `
 
 const Header = () => {
   return (
     <MyHeader>
+      <LanguageToggle></LanguageToggle>
       <LogoImg src="/img/veggie-bistro-logo.png" alt="" />
     </MyHeader>
   )
---
+++ src/components/ui/LanguageToggle.tsx
@@ -0,0 +1,46 @@
+import styled from 'styled-components'
+import { Locale, availableLocales, setLocale } from '../../store/UISlice'
+import { useDispatch } from 'react-redux'
+import { setMenu } from '../../store/menuSlice'
+
+const MyLanguageToggle = styled.div`
+  position: absolute;
+  top: 1em;
+  left: 1em;
+  display: flex;
+  gap: 1em;
+
+  div {
+    cursor: pointer;
+    transition: transform 0.2s;
+
+    &:active {
+      transform: scale(0.9);
+    }
+  }
+`
+
+const LanguageToggle = () => {
+  const dispatch = useDispatch()
+  const handleChangeLocale = (locale: string) => {
+    dispatch(setMenu(locale as Locale))
+    dispatch(setLocale(locale as Locale))
+  }
+
+  return (
+    <MyLanguageToggle>
+      {availableLocales.map((locale) => (
+        <div
+          onClick={() => {
+            handleChangeLocale(locale)
+          }}
+          key={locale}
+        >
+          {locale} {locale === 'pt' ? '🇧🇷' : '🇺🇸'}
+        </div>
+      ))}
+    </MyLanguageToggle>
+  )
+}
+
+export default LanguageToggle
