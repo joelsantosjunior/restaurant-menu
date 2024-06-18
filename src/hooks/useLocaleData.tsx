f2f9f22503a5a74418fb0f7bed39686138d67071
Joel Santos <joel.junior@actdigital.com>
2024-06-18 14:04:26 -0300

    fix hook

--- src/hooks/useLocaleData.tsx
+++ src/hooks/useLocaleData.tsx
@@ -1,9 +1,10 @@
+import { useSelector } from 'react-redux'
 import { availableLocales } from '../store/UISlice'
 import { LocaleObject, localeDictionary } from '../utils/locale.utils'
-import useLocale from './useLocale'
+import { AppState } from '../store/store'
 
 const useLocaleData = (): ((key: keyof LocaleObject) => string) => {
-  let locale = useLocale()
+  let locale = useSelector((state: AppState) => state.ui.locale)
 
   if (!availableLocales.includes(locale)) {
     locale = 'en'
