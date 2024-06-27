a3cf09ceadfe536ba4c39d0eed70a1f513552451
Joel Santos <joel.junior@actdigital.com>
2024-06-27 15:15:21 -0300

    load from remote API

--- src/api/rest-api.ts
+++ src/api/rest-api.ts
@@ -6,7 +6,7 @@ const BASE_URL = 'https://cdn-dev.preoday.com/challenge'
 const PROXY_SERVER = `http://localhost:3222/proxy?url=${BASE_URL}`
 
 export const getRestaurantData = async (): Promise<Venue> => {
-  const response = await fetch(`${PROXY_SERVER}/venue/9`)
+  const response = await fetch(`${BASE_URL}/venue/9`)
 
   if (!response.ok) {
     throw new Error('Failed to fetch restaurant data')
@@ -18,7 +18,7 @@ export const getRestaurantData = async (): Promise<Venue> => {
 }
 
 export const getMenuData = async (): Promise<Menu> => {
-  const response = await fetch(`${PROXY_SERVER}/menu`)
+  const response = await fetch(`${BASE_URL}/menu`)
 
   if (!response.ok) {
     throw new Error('Failed to fetch menu data')
