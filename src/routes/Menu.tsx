b21d7c763373e52d2d88bcedb527053a3c99fee2
Joel Santos <joel.junior@actdigital.com>
2024-06-27 10:04:07 -0300

    feat: implement search

--- src/routes/Menu.tsx
+++ src/routes/Menu.tsx
@@ -11,6 +11,8 @@ import { useSelector, useDispatch } from 'react-redux'
 import { AppState } from '../store/store'
 import Basket from '../components/layout/Basket'
 import LoadingSpinner from '../components/ui/loading-spinner/LoadingSpinner'
+import { useState } from 'react'
+import { MenuSection as MenuSectionModel } from '../models/MenuSection.model'
 
 const MenuSection = () => {
   const { data, status } = useQuery({
@@ -22,30 +24,52 @@ const MenuSection = () => {
     (state: AppState) => state.ui.showOrderModel
   )
 
+  const [filteredItems, setFilteredItems] = useState<MenuSectionModel[]>([])
+
   const dispatch = useDispatch()
 
   const categories =
     data?.sections.sort((a, b) => a.position - b.position) || []
 
-  const sections = categories.map((category) => (
-    <MenuCategory
-      key={category.id}
-      quickAccessId={category.name}
-      category={category.name}
-      items={category.items}
-    />
-  ))
-
   if (status === 'pending') {
     return <LoadingSpinner></LoadingSpinner>
+  }
+
+  const handleSearch = (searchText: string) => {
+    if (searchText === '') {
+      setFilteredItems(categories)
+      return
+    }
+
+    const filteredItems = categories
+      .map((category) => {
+        return {
+          ...category,
+          items: category.items.filter(
+            (item) =>
+              item.name.toLowerCase().includes(searchText.toLowerCase()) ||
+              item.description?.toLowerCase().includes(searchText.toLowerCase())
+          ),
+        }
+      })
+      .filter((category) => category.items.length > 0)
+
+    setFilteredItems(filteredItems)
   }
 
   return (
     <>
       <div className={styles.menuContainer}>
-        <TextField onChange={() => {}}></TextField>
+        <TextField onChange={handleSearch}></TextField>
         <CategoriesCarousel data={categories}></CategoriesCarousel>
-        {sections}
+        {filteredItems.map((category) => (
+          <MenuCategory
+            key={category.id}
+            quickAccessId={category.name}
+            category={category.name}
+            items={category.items}
+          />
+        ))}
         {showOrderModal && (
           <Modal>
             <OrderModal
